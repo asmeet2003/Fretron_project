611657ce6c1711f08e9f28810c5eede2af8a9564
Asmeet Arora <asmeet0011@gmail.com>
2024-08-02 12:26:03 +0530

    Create App.js

---
+++ project1/frontend/src/App.js
@@ -0,0 +1,16 @@
+ 
+import React from 'react';
+import FlightPathChart from './FlightPathChart';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Flight Path Visualization</h1>
+        <FlightPathChart />
+      </header>
+    </div>
+  );
+}
+
+export default App;
